338a855b0e81edb11b2aae3077b504bc9cf77060
Roshni1998 <roshnimali847@gmail.com>
2021-07-07 18:25:36 +0530

    Step-2 Multiple Rides

--- src/main/java/com/bridgelabz/utils/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/utils/InvoiceGenerator.java
@@ -4,9 +4,10 @@ public class InvoiceGenerator {
 
     private static final double MINIMUM_COST_PER_KM = 10;
     private static final int COST_PER_MINUTE = 1;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
         double totalFare = (distance * MINIMUM_COST_PER_KM) + (time * COST_PER_MINUTE);
-        return totalFare;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
